257c769e6804b2ff1347d19f01b9bdc69299d752
Mr.None <thewitcher745@gmail.com>
2026-03-08 20:01:39 +0330

    FEAT: Added base_candle_time to block dict representatiion

--- src/logic/blocks/block.py
+++ src/logic/blocks/block.py
@@ -1,6 +1,7 @@
 from abc import ABC
 from typing import Any, Dict
-from pandas import Timestamp
+from pandas import DataFrame, Timestamp
+import numpy as np
 
 
 class Block(ABC):
@@ -17,18 +18,40 @@ class Block(ABC):
         low: float,
         high: float,
         start_time: Timestamp,
+        invalidation_price: float,
     ):
-
         self.base_candle_index = base_candle_index
         self.base_candle_time = base_candle_time
-        self.direction = direction  # 'long' or 'short'
+        self.direction = direction  # 'bullish' or 'bearish'
         self.block_type = block_type  # 'OB', 'BB', or 'MB'
         self.low = low
         self.high = high
         self.start_time = start_time
         self.end_time = None
+        self.end_index = None
+        self.invalidation_price = invalidation_price
 
         self.id = f"{'Bu' if self.direction == 'bullish' else 'Be'}_{self.block_type}_{base_candle_time.strftime('%Y-%m-%dT%H:%M:%S')}"
+
+    def check_end_candle(self, klines_df: DataFrame):
+        """
+        This method checks if the price has closed below/above the invalidation price.
+        """
+        closes_after_start: np.array = klines_df.iloc[self.base_candle_index :].close
+
+        if self.direction == "bullish":
+            invalidating_candles = np.where(
+                closes_after_start < self.invalidation_price
+            )[0]
+            if len(invalidating_candles):
+                return invalidating_candles[0] + self.base_candle_index
+
+        else:
+            invalidating_candles = np.where(
+                closes_after_start > self.invalidation_price
+            )[0]
+            if len(invalidating_candles):
+                return invalidating_candles[0] + self.base_candle_index
 
     def to_dict(self) -> Dict[str, Any]:
         """Utility for converting to a DataFrame-ready format."""
@@ -40,5 +63,6 @@ class Block(ABC):
             "high": self.high,
             "start_time": self.start_time,
             "base_candle_index": self.base_candle_index,
+            "base_candle_time": self.base_candle_time,
             "end_time": self.end_time,
         }
