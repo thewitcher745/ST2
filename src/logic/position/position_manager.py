2a8240f4ae722c2abff9283a33928d7e3f2b0373
Mohammad Amin Seifinia <thewitcher745@gmail.com>
2026-04-18 09:24:25 +0000

    REF: Changed PositionManager.all_positions to .positions and removed active_positions

--- src/logic/position/position_manager.py
+++ src/logic/position/position_manager.py
@@ -8,34 +8,24 @@ from ..backtest import PositionSimulator
 
 class PositionManager:
     def __init__(self):
-        self.all_positions: dict[str, list[Position]] = {"long": [], "short": []}
-        self.active_positions: dict[str, list[Position]] = {"long": [], "short": []}
+        self.positions: dict[str, list[Position]] = {"long": [], "short": []}
 
-    def update_positions(self, blocks: list[Block], active=False):
+    def update_positions(self, blocks: list[Block]):
         """
         Updates the list of positions from a given list of blocks, direction-agnostic.
         """
-        all_positions: dict[str, list[Position]] = {"long": [], "short": []}
-        active_positions: dict[str, list[Position]] = {"long": [], "short": []}
+        positions: dict[str, list[Position]] = {"long": [], "short": []}
         for block in blocks:
             type = "long" if block.direction == "bullish" else "short"
-            if active:
-                active_positions[type].append(Position(block))
-            else:
-                all_positions[type].append(Position(block))
+            positions[type].append(Position(block))
 
-        if active:
-            self.active_positions = active_positions
-        else:
-            self.all_positions = all_positions
+        self.positions = positions
 
     @property
-    def all_active_positions(self) -> list[Position]:
-        """Takes a long/hort separated dict of positions and returns a combined dict."""
+    def positions_aslist(self) -> list[Position]:
+        """Takes a long/hort separated dict of positions and returns a combined list."""
         return [
-            position
-            for position in self.active_positions["long"]
-            + self.active_positions["short"]
+            position for position in self.positions["long"] + self.positions["short"]
         ]
 
     def simulate_all_positions(self, klines_data: KLinesData):
@@ -44,13 +34,13 @@ class PositionManager:
         profits.
         """
         for type in ["short", "long"]:
-            for position in self.all_positions[type]:
+            for position in self.positions[type]:
                 PositionSimulator.simulate(position, klines_data)
 
     def to_dataframe(self) -> DataFrame:
         return DataFrame(
             [
                 position.to_dict()
-                for position in self.all_positions["long"] + self.all_positions["short"]
+                for position in self.positions["long"] + self.positions["short"]
             ]
         )
